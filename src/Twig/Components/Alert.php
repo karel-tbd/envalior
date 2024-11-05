c007ee7bb0f718ee0e90ce06f08f288830363a9b
karel-tbd <karel@tbd.be>
2024-11-05 17:12:43 +0100

    made homepage

---
+++ src/Twig/Components/Alert.php
@@ -0,0 +1,12 @@
+<?php
+// src/Twig/Components/Alert.php
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent]
+class Alert
+{
+    public string $message;
+    public string $type = 'success';
+}
